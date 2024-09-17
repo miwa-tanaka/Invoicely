2c726e8f3298bd843d999e27736091bc74b44e84
Miwa <miwa.tnk9675@gmail.com>
2024-09-17 22:20:55 +0900

    delete: unnecessary files

--- src/components/atoms/cartIcon.tsx
+++
@@ -1,5 +0,0 @@
-import { IoBagHandleOutline } from "react-icons/io5";
-
-export default function CartIcon(): JSX.Element {
-  return <IoBagHandleOutline role="img" aria-label="quantity in cart" />;
-}
--- src/components/organisms/footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/footer.module.scss";
-
-export default function Footer(): JSX.Element {
-  return (
-    <footer className={styles.footer}>
-      <small>&#169; Blossom Avenue</small>
-    </footer>
-  );
-}
