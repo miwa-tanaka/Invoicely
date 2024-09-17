c1687ba4bed45e45db256b89a18303e35beb74d0
Miwa <miwa.tnk9675@gmail.com>
2024-09-17 22:17:27 +0900

    modify: updated header

--- src/components/atoms/logo.tsx
+++ src/components/atoms/logo.tsx
@@ -1,8 +1,11 @@
-import style from "@/styles/logo.module.scss";
+import { css } from "@emotion/react";
+import { Box, useTheme } from "@chakra-ui/react";
 
 export default function Logo(): JSX.Element {
+  const { wrapper } = LogoStyles();
+
   return (
-    <div className={style.wrapper}>
+    <Box css={wrapper} aria-label="logo: Invoicely">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="100"
@@ -11,12 +14,64 @@ export default function Logo(): JSX.Element {
       >
         <path
           fill="#FFF"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z"
           role="img"
           aria-hidden={true}
         />
       </svg>
-    </div>
+    </Box>
   );
 }
+
+export const LogoStyles = () => {
+  const { colors, radii, breakpoints } = useTheme();
+
+  return {
+    wrapper: css`
+      position: relative;
+      width: 100px;
+      height: 100px;
+      overflow: hidden;
+      background-color: ${colors.purple[1]};
+      border-radius: 0 ${radii["2xl"]} ${radii["2xl"]} 0;
+
+      @media screen and (max-width: ${breakpoints["md"]}) {
+        width: 80px;
+        height: 80px;
+      }
+
+      &::before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 50%;
+        content: "";
+        background-color: ${colors.purple[1]};
+        border-radius: 0 ${radii["2xl"]} ${radii["2xl"]} 0;
+      }
+
+      &::after {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 50%;
+        content: "";
+        background-color: ${colors.purple[2]};
+        border-radius: ${radii["2xl"]} 0 0 0;
+      }
+
+      svg {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        z-index: 1;
+        width: 40%;
+        height: 40%;
+        transform: translate(-50%, -50%);
+      }
+    `,
+  };
+};
--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,22 +1,80 @@
-import Link from "next/link";
-import styles from "@/styles/header.module.scss";
-import ItemsInCart from "@/components/molecules/itemsInCart";
+import { Box, Flex, useTheme } from "@chakra-ui/react";
+import { css } from "@emotion/react";
+import Logo from "@/components/atoms/logo";
+import ThemeChangeButton from "@/components/atoms/themeChangeButton";
 
 export default function Header(): JSX.Element {
+  const { wrapper, logoWrapper, userWrapper, user } = HeaderStyles();
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        <img
-          src="/images/icon.png"
-          alt="Logo: Blossom Avenue"
-          width={45}
-          height={45}
-        />
-        Blossom Avenue
-      </Link>
-      <div className={styles.icon}>
-        <ItemsInCart />
-      </div>
-    </header>
+    <Flex as="header" css={wrapper}>
+      <Flex css={logoWrapper}>
+        <Logo />
+        <ThemeChangeButton />
+      </Flex>
+      <Flex css={userWrapper}>
+        <Box css={user}>
+          <img
+            src="/images/image-avatar.jpg"
+            alt="headshot: John Doe"
+            width={40}
+            height={40}
+          />
+        </Box>
+      </Flex>
+    </Flex>
   );
 }
+
+export const HeaderStyles = () => {
+  const { colors, radii, space, breakpoints } = useTheme();
+
+  return {
+    wrapper: css`
+      flex-direction: column;
+      justify-content: space-between;
+      width: 100px;
+      height: 100svh;
+      background-color: ${colors.navy[1]};
+      border-radius: 0 ${radii["2xl"]} ${radii["2xl"]} 0;
+
+      @media screen and (max-width: ${breakpoints["md"]}) {
+        width: 100%;
+        height: 80px;
+        flex-direction: row;
+        border-radius: 0;
+      }
+    `,
+    logoWrapper: css`
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+      margin-bottom: ${space[6]};
+
+      @media screen and (max-width: ${breakpoints["md"]}) {
+        align-items: center;
+        flex-direction: row;
+        margin-right: ${space[6]};
+        margin-bottom: 0;
+        width: 100%;
+      }
+    `,
+    userWrapper: css`
+      padding: ${space[6]} 0;
+      border-top: 1px solid ${colors.gray[4]};
+      align-items: center;
+      justify-content: center;
+
+      @media screen and (max-width: ${breakpoints["md"]}) {
+        padding: 0 ${space[6]};
+        border-top: none;
+        border-left: 1px solid ${colors.gray[4]};
+      }
+    `,
+    user: css`
+      width: 40px;
+      height: 40px;
+      border-radius: ${radii["full"]};
+      overflow: hidden;
+    `,
+  };
+};
