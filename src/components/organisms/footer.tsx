9b004c632b0e73828dea82f38a6a2531b87946b8
Miwa <miwa.tnk9675@gmail.com>
2024-09-17 14:43:13 +0900

    modify: created footer

---
+++ src/components/organisms/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/footer.module.scss";
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className={styles.footer}>
+      <small>&#169; Blossom Avenue</small>
+    </footer>
+  );
+}
