8e1adc997444ef8cce7aad96d526c7fcfbaa5598
Miwa <miwa.tnk9675@gmail.com>
2024-09-23 16:20:57 +0900

    add: plus icon

---
+++ src/components/atoms/icons/plusIcon.tsx
@@ -0,0 +1,18 @@
+export default function PlusIcon(): JSX.Element {
+  return (
+    <svg
+      width="11"
+      height="11"
+      viewBox="0 0 11 11"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-hidden={true}
+    >
+      <path
+        d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z"
+        fill="#7C5DFA"
+        fillRule="nonzero"
+      />
+    </svg>
+  );
+}
