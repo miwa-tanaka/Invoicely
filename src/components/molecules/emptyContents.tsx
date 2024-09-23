6917f3cdaa53677f02cfdea6a7a6d895e06fe6ff
Miwa <miwa.tnk9675@gmail.com>
2024-09-23 18:26:49 +0900

    add: created EmptyContents

---
+++ src/components/molecules/emptyContents.tsx
@@ -0,0 +1,42 @@
+import { Flex, Heading, Text, useTheme, useColorMode } from "@chakra-ui/react";
+import { css } from "@emotion/react";
+import { useContentHeight } from "@/hooks/useContentHeight";
+
+export default function EmptyContents(): JSX.Element {
+  const { heading, text } = useEmptyContentsStyles();
+  const { contentHeight } = useContentHeight();
+
+  return (
+    <Flex direction="column" justify="center" align="center" h={contentHeight}>
+      <img src="/images/empty-illust.png" alt="" width={241} height={200} />
+      <Heading as="h2" mt={16} mb={6} fontSize="2xl" css={heading}>
+        There is nothing here
+      </Heading>
+      <Text css={text}>
+        Create an invoice by clicking the
+        <br />
+        <Text as="span" fontWeight={700}>
+          {" "}
+          New Invoice{" "}
+        </Text>
+        button and get started
+      </Text>
+    </Flex>
+  );
+}
+
+export const useEmptyContentsStyles = () => {
+  const { colors } = useTheme();
+  const { colorMode } = useColorMode();
+
+  return {
+    heading: css`
+      font-family: unset;
+      color: ${colorMode === "light" ? colors.black[1] : colors.white[1]};
+    `,
+    text: css`
+      color: ${colorMode === "light" ? colors.gray[2] : colors.white[1]};
+      text-align: center;
+    `,
+  };
+};
