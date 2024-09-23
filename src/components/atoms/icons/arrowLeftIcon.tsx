35f20b6d4f04536a75b9f667edc57c3d0c1614a7
Miwa <miwa.tnk9675@gmail.com>
2024-09-23 16:18:46 +0900

    add: arrow icons

---
+++ src/components/atoms/icons/arrowLeftIcon.tsx
@@ -0,0 +1,20 @@
+export default function ArrowLeftIcon(): JSX.Element {
+  return (
+    <svg
+      width="7"
+      height="10"
+      viewBox="0 0 7 10"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-hidden={true}
+    >
+      <path
+        d="M6.342.886L2.114 5.114l4.228 4.228"
+        stroke="#9277FF"
+        strokeWidth="2"
+        fill="none"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}
