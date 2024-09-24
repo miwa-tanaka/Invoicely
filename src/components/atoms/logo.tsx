0d484e12ce2239fe91772943841431ae7c2e32a1
Miwa <miwa.tnk9675@gmail.com>
2024-09-24 21:24:46 +0900

    modify: logo style

--- src/components/atoms/logo.tsx
+++ src/components/atoms/logo.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
 import { Box, useTheme } from "@chakra-ui/react";
+import { useHeaderSize } from "@/hooks/useHeaderSize";
 
 export default function Logo(): JSX.Element {
   const { wrapper } = LogoStyles();
@@ -11,13 +12,13 @@ export default function Logo(): JSX.Element {
         width="100"
         height="100"
         viewBox="0 0 28 26"
+        role="img"
+        aria-hidden={true}
       >
         <path
           fill="#FFF"
           fillRule="evenodd"
           d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z"
-          role="img"
-          aria-hidden={true}
         />
       </svg>
     </Box>
@@ -25,51 +26,47 @@ export default function Logo(): JSX.Element {
 }
 
 export const LogoStyles = () => {
-  const { colors, radii, breakpoints } = useTheme();
+  const { colors, radii } = useTheme();
+  const { headerSize } = useHeaderSize();
 
   return {
     wrapper: css`
+      width: ${headerSize};
+      height: ${headerSize};
       position: relative;
-      width: 100px;
-      height: 100px;
       overflow: hidden;
       background-color: ${colors.purple[1]};
       border-radius: 0 ${radii["2xl"]} ${radii["2xl"]} 0;
 
-      @media screen and (max-width: ${breakpoints["md"]}) {
-        width: 80px;
-        height: 80px;
-      }
-
       &::before {
+        content: "";
+        width: 100%;
+        height: 50%;
         position: absolute;
         top: 0;
         left: 0;
-        width: 100%;
-        height: 50%;
-        content: "";
         background-color: ${colors.purple[1]};
         border-radius: 0 ${radii["2xl"]} ${radii["2xl"]} 0;
       }
 
       &::after {
+        content: "";
+        width: 100%;
+        height: 50%;
         position: absolute;
         bottom: 0;
         left: 0;
-        width: 100%;
-        height: 50%;
-        content: "";
         background-color: ${colors.purple[2]};
         border-radius: ${radii["2xl"]} 0 0 0;
       }
 
       svg {
+        width: 40%;
+        height: 40%;
         position: absolute;
         top: 50%;
         left: 50%;
         z-index: 1;
-        width: 40%;
-        height: 40%;
         transform: translate(-50%, -50%);
       }
     `,
