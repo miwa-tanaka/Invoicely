61a361175a98566dff0bb2e9f75a63ed72aef27a
Miwa <miwa.tnk9675@gmail.com>
2024-09-24 21:32:02 +0900

    add: data type

---
+++ src/data/dataType.ts
@@ -0,0 +1,26 @@
+export type dataType = {
+  id: string;
+  createdAt: string;
+  paymentDue: string;
+  description: string;
+  paymentTerms: number;
+  clientName: string;
+  clientEmail: string;
+  status: "pending" | "paid" | "draft";
+  senderAddress: {
+    street: string;
+    city: string;
+    postCode: string;
+    country: string;
+  };
+  clientAddress: {
+    street: string;
+    city: string;
+    postCode: string;
+    country: string;
+  };
+  items: object[];
+  total: number;
+};
+
+export type jsonDataType = dataType[];
