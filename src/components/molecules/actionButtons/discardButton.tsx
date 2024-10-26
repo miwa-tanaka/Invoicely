462f3b21611688bd7e3884db8ef8b03ad16060fd
Miwa <miwa.tnk9675@gmail.com>
2024-10-26 14:01:40 +0900

    modify: created discard button

---
+++ src/components/molecules/actionButtons/discardButton.tsx
@@ -0,0 +1,50 @@
+import { Flex, useTheme, useColorMode } from "@chakra-ui/react";
+import { css } from "@emotion/react";
+import ButtonBase from "@/components/atoms/buttonBase";
+
+export type discardButtonProps = {
+  onClick: () => void;
+  state: "new" | "edit";
+};
+
+export default function DiscardButton({
+  onClick,
+  state,
+}: discardButtonProps): JSX.Element {
+  const { discardButton } = useDiscardButtonStyles();
+
+  return (
+    <>
+      {state === "new" && (
+        <Flex>
+          <ButtonBase
+            text="Discard"
+            onClick={onClick}
+            style={discardButton}
+            aria-label="Discard changes"
+          />
+        </Flex>
+      )}
+    </>
+  );
+}
+
+export const useDiscardButtonStyles = () => {
+  const { colors } = useTheme();
+  const { colorMode } = useColorMode();
+
+  return {
+    discardButton: css`
+      background-color: ${colorMode === "light"
+        ? colors.white[2]
+        : colors.navy[2]};
+      font-weight: 700;
+      color: ${colorMode === "light" ? colors.gray[3] : colors.gray[1]};
+
+      &:hover {
+        background-color: ${colorMode === "light" ? colors.gray[1] : "white"};
+        color: ${colorMode === "light" ? colors.gray[3] : colors.gray[1]};
+      }
+    `,
+  };
+};
