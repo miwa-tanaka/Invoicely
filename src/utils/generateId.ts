6b622d78b736d2143fa0e302eb2b615385cd0506
Miwa <miwa.tnk9675@gmail.com>
2024-10-31 08:00:41 +0900

    add: created generate ID function

---
+++ src/utils/generateId.ts
@@ -0,0 +1,35 @@
+import type { idListType } from "@/data/dataType";
+
+/**
+ * Generates a random ID with a specified format.
+ * ID consists of 2 uppercase letters followed by 4 digits.
+ *
+ * @returns {string} The generated ID.
+ */
+const generateRandomId = () => {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+
+  let returnId = "";
+
+  for (let i = 0; i < 2; i++) {
+    returnId += letters.charAt(Math.floor(Math.random() * letters.length));
+  }
+
+  for (let i = 0; i < 4; i++) {
+    returnId += numbers.charAt(Math.floor(Math.random() * numbers.length));
+  }
+
+  return returnId;
+};
+
+export const generateUniqueId = (existingIds: idListType) => {
+  let newId = generateRandomId();
+
+  // check if id is unique
+  while (existingIds.includes(newId)) {
+    newId = generateRandomId();
+  }
+
+  return newId;
+};
