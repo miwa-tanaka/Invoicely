b6f08c53c4a654570415f3fc9880d716f494667d
Miwa <miwa.tnk9675@gmail.com>
2024-11-08 10:57:15 +0900

    modify: margin change for phone size

--- src/hooks/useContentWidth.ts
+++ src/hooks/useContentWidth.ts
@@ -1,14 +1,19 @@
 import { useTheme } from "@chakra-ui/react";
 import { useHeaderSize } from "./useHeaderSize";
 import { useIsLargerThanTabletSize } from "@/hooks/useIsLargerThanTabletSize";
+import { useIsLargerThanPhoneSize } from "@/hooks/useIsLargerThanPhoneSize";
 
 export const useContentHeight = () => {
   const { space } = useTheme();
   const { headerSize } = useHeaderSize();
   const isLargerThanTabletSize = useIsLargerThanTabletSize();
-
+  const isLargerThanPhoneSize = useIsLargerThanPhoneSize();
   const contentHeaderHeight = "5rem";
-  const contentMarginTop = isLargerThanTabletSize ? space[16] : space[14];
+  const contentMarginTop = isLargerThanTabletSize
+    ? space[16]
+    : isLargerThanPhoneSize
+    ? space[10]
+    : space[8];
   const contentHeight = isLargerThanTabletSize
     ? `calc(100svh - ${contentHeaderHeight} - ${contentMarginTop})`
     : `calc(100svh - ${headerSize} - ${contentHeaderHeight} - ${contentMarginTop})`;
