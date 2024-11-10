368cc00df222279926f06a591ec56d9003218ed7
Miwa <miwa.tnk9675@gmail.com>
2024-11-10 21:00:33 +0900

    add: created status heading

--- src/components/molecules/itemDetails/index.tsx
+++ src/components/molecules/itemDetails/index.tsx
@@ -1,25 +1,47 @@
 import NextLink from "next/link";
-import { Link, Flex, Text, useTheme, useColorMode } from "@chakra-ui/react";
+import { Link, Flex, useTheme, useColorMode } from "@chakra-ui/react";
 import { css } from "@emotion/react";
 import ArrowLeftIcon from "@/components/atoms/icons/arrowLeftIcon";
-import StatusButtons from "@/components/molecules/itemDetails/statusButtons";
 import type { dataType } from "@/data/dataType";
 import DetailTable from "@/components/molecules/itemDetails/detailTable";
 import Price from "@/components/molecules/itemDetails/price/index";
+import StatusHeading from "@/components/molecules/itemDetails/statusHeading";
+import { useContentHeight } from "@/hooks/useContentWidth";
+import { useHeaderSize } from "@/hooks/useHeaderSize";
+import { useIsLargerThanTabletSize } from "@/hooks/useIsLargerThanTabletSize";
+import { useIsLargerThanPhoneSize } from "@/hooks/useIsLargerThanPhoneSize";
 
-type itemDetailsProps = { data: dataType };
+type itemDetailsProps = {
+  data: dataType;
+  onStatusUpdate: () => void;
+  isDeleteModalOpen: boolean;
+  onDeleteModalOpen: () => void;
+  onDeleteModalClose: () => void;
+};
 
-export default function ItemDetails({ data }: itemDetailsProps): JSX.Element {
-  const { link, wrapper } = useItemDetailsStyles();
-  console.log(data, "data");
+export default function ItemDetails({
+  data,
+  onStatusUpdate,
+  isDeleteModalOpen,
+  onDeleteModalOpen,
+  onDeleteModalClose,
+}: itemDetailsProps): JSX.Element {
+  const { contesWrapper, link, wrapper } = useItemDetailsStyles();
 
   return (
-    <Flex direction="column" gap={6}>
+    <Flex direction="column" gap={6} css={contesWrapper}>
       <Link as={NextLink} href="/" css={link}>
         <ArrowLeftIcon />
         Go back
       </Link>
-      <StatusButtons status={data.status} id={data.id} />
+      <StatusHeading
+        status={data.status}
+        id={data.id}
+        onStatusUpdate={onStatusUpdate}
+        isDeleteModalOpen={isDeleteModalOpen}
+        onDeleteModalOpen={onDeleteModalOpen}
+        onDeleteModalClose={onDeleteModalClose}
+      />
       <Flex css={wrapper}>
         <DetailTable
           id={data.id}
@@ -38,11 +60,27 @@ export default function ItemDetails({ data }: itemDetailsProps): JSX.Element {
 }
 
 export const useItemDetailsStyles = () => {
-  const { colors, space, radii } = useTheme();
+  const { colors, space, radii, breakpoints } = useTheme();
   const { colorMode } = useColorMode();
+  const { contentMarginTop } = useContentHeight();
+  const isLargerThanTabletSize = useIsLargerThanTabletSize();
+  const isLargerThanPhoneSize = useIsLargerThanPhoneSize();
+  const { headerSize, statusHeadingButtonHeight } = useHeaderSize();
+
+  const listHeight = isLargerThanTabletSize
+    ? `calc(100svh - ${contentMarginTop})`
+    : isLargerThanPhoneSize
+    ? `calc(100svh - ${headerSize} - ${contentMarginTop})`
+    : `calc(100svh - ${headerSize} - ${contentMarginTop} - ${statusHeadingButtonHeight})`;
 
   return {
+    contesWrapper: css`
+      height: ${listHeight};
+      overflow-y: auto;
+      width: 100%;
+    `,
     link: css`
+      width: fit-content;
       display: flex;
       align-items: center;
       gap: ${space[5]};
@@ -57,12 +95,16 @@ export const useItemDetailsStyles = () => {
     `,
     wrapper: css`
       width: 100%;
-      padding: ${space[12]} ${space[10]};
+      padding: ${space[6]};
       align-items: center;
       flex-direction: column;
       gap: ${space[10]};
       border-radius: ${radii["md"]};
       background-color: ${colorMode === "light" ? "white" : colors.navy[1]};
+
+      @media screen and (min-width: ${breakpoints["md"]}) {
+        padding: ${space[12]} ${space[10]};
+      }
     `,
   };
 };
