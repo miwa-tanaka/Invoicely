b15b9d596d73a0713a3ed7c3262ad0b1461b8ec7
Miwa <miwa.tnk9675@gmail.com>
2024-11-10 20:41:30 +0900

    modify: detail page - added drawer

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,31 +1,73 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import type { dataType } from "@/data/dataType";
+import { useDisclosure } from "@chakra-ui/react";
 import ContentsWrapper from "@/components/templates/contentsWrapper";
 import ItemDetails from "@/components/molecules/itemDetails";
+import Drawer from "@/components/organisms/drawer";
+import { useDrawer } from "@/context/drawerContext";
+import StatusHeadingButtons from "@/components/molecules/itemDetails/statusHeadingButtons";
+import { useIsLargerThanPhoneSize } from "@/hooks/useIsLargerThanPhoneSize";
 
 const ItemDetailPage = () => {
   const { id } = useParams();
   const [item, setItem] = useState<dataType | undefined>();
+  const { isOpen, onClose } = useDrawer();
+  const {
+    isOpen: isDeleteModalOpen,
+    onOpen: onDeleteModalOpen,
+    onClose: onDeleteModalClose,
+  } = useDisclosure();
+  const isLargerThanPhoneSize = useIsLargerThanPhoneSize();
 
-  useEffect(() => {
-    // Retrieve data from localStorage
+  const retrieveData = useCallback(() => {
     const storedData = localStorage.getItem("invoices");
 
     if (storedData) {
       const itemsData: dataType[] = JSON.parse(storedData);
-      // Find the item by id
-      const foundItem = itemsData.find((item) => item.id === id);
+      const foundItem = itemsData.find((invoice) => invoice.id === id);
       setItem(foundItem);
     }
   }, [id]);
 
-  console.log(item, "item");
+  useEffect(() => {
+    retrieveData();
+  }, [retrieveData]);
 
   return (
-    <ContentsWrapper>{item && <ItemDetails data={item} />}</ContentsWrapper>
+    <>
+      <ContentsWrapper>
+        {item && (
+          <>
+            <ItemDetails
+              data={item}
+              onStatusUpdate={retrieveData}
+              isDeleteModalOpen={isDeleteModalOpen}
+              onDeleteModalOpen={onDeleteModalOpen}
+              onDeleteModalClose={onDeleteModalClose}
+            />
+            <Drawer
+              state="edit"
+              isOpen={isOpen}
+              onClose={onClose}
+              updateInvoices={retrieveData}
+              id={item.id}
+              data={item}
+            />
+          </>
+        )}
+      </ContentsWrapper>
+      {item && !isLargerThanPhoneSize && (
+        <StatusHeadingButtons
+          status={item.status}
+          id={item.id}
+          onStatusUpdate={retrieveData}
+          onDeleteModalOpen={onDeleteModalOpen}
+        />
+      )}
+    </>
   );
 };
 
